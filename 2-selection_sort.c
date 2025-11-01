d54eab853c0e327d8f87136e8c5b8f7bbf9c0f79
user76335 <11958@holbertonstudents.com>
2025-11-01 17:59:59 +0000

    update 2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -2,30 +2,34 @@
 
 /**
  * selection_sort - sorts an array of integers in ascending order
- * @array: array to sort
- * @size: number of elements
+ * using the Selection sort algorithm
+ * @array: array of integers
+ * @size: size of the array
  */
 void selection_sort(int *array, size_t size)
 {
-    size_t i, j, min_idx;
-    int tmp;
-    if (!array || size < 2)
-        return;
+	size_t i, j, min_idx;
+	int temp;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        min_idx = i;
-        for (j = i + 1; j < size; j++)
-        {
-            if (array[j] < array[min_idx])
-                min_idx = j;
-        }
-        if (min_idx != i)
-        {
-            tmp = array[i];
-            array[i] = array[min_idx];
-            array[min_idx] = tmp;
-            print_array(array, size);
-        }
-    }
+	if (!array || size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min_idx = i;
+
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min_idx])
+				min_idx = j;
+		}
+
+		if (min_idx != i)
+		{
+			temp = array[i];
+			array[i] = array[min_idx];
+			array[min_idx] = temp;
+			print_array(array, size);
+		}
+	}
 }
